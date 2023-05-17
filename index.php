66487205adb0adbdf485e1831b1d8bd7ff98346a
NelloRossi <rossianiello97@gmail.com>
2023-05-17 13:36:57 +0200

    scaffholding

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+include __DIR__ . '/data/db.php';
+?>
+
+<?php include __DIR__.'/partials/template/head.php' ?>
+
+
+<main>
+    <div class="container">
+        
+    </div>
+
+</main>
+
+<?php include __DIR__.'/partials/template/foot.php' ?>
